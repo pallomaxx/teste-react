e6bf216159cb0f49e8fb74552bee32f2b1b1467d
pallomaxx <pallomaneves2011@gmail.com>
2024-11-26 17:19:27 -0300

    correcoes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router basename="/teste-react">
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
